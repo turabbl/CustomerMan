86e295a395297a950abcbb1e314937ca87c44e14
turabbl <turabbl@gmail.com>
2021-09-30 05:03:05 +0400

    Customer

---
+++ src/main/java/com/proje/customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.proje.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+}
